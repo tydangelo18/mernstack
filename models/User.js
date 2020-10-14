e670a92f14b2860cbb39d27c99250a37b2a28246
Ty D'Angelo <tydangelo18@gmail.com>
2020-10-14 16:27:15 -0500

    Created User Model.

---
+++ models/User.js
@@ -0,0 +1,29 @@
+// Bring in Mongoose
+const mongoose = require('mongoose');
+
+// Create a User Schema
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Export Schema to be used
+module.exports = User = mongoose.model('user', UserSchema);
