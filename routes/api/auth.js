1d58b67b577535411645ed1eb8f5ec7b64715915
Ty D'Angelo <tydangelo18@gmail.com>
2020-10-20 17:23:19 -0500

    Build and test login route.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,6 +5,11 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 // Bring in User Model
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+// Bring in Express Validator
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 // @route GET api/auth
 // @desc Test Route
@@ -17,6 +22,63 @@ router.get('/', auth, async (req, res) => {
         console.error(err.message);
         res.status(500).send('Server Error');
     }
+});
+
+// @route POST api/auth
+// @desc Authenticate User & get Token
+// @access Public
+router.post('/', 
+[
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+],
+ async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+    // See if the User exists
+    let user = await User.findOne({ email });
+
+    if(!user) {
+        return res.status(400).json({ errors: [ { msg: 'Invalid credentials' }] });
+    }
+
+    // Match email and password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch) {
+        return res.status(400).json({ errors: [ { msg: 'Invalid credentials' }] });
+    }
+
+        // Return the jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+                } 
+            );  
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+
+    
+
+    
 });
 
 // Export Route
