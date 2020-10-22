27f52ff8ca1cdae921c878155f2d1601d15371c2
Ty D'Angelo <tydangelo18@gmail.com>
2020-10-22 16:05:17 -0500

    Built route to delete a profile and user.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -137,6 +137,25 @@ router.get('/user/:user_id', async (req, res) => {
     }
 });
 
+//@route DELETE api/profile
+//@desc Delete profile, user & posts
+//@access Private
+router.delete('/', auth, async (req, res) => {
+    try {
+        // @todo --> remove user's posts
+
+        // Remove Profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+        // Remove User
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({ msg: 'User deleted' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 
 // Export Route
 module.exports = router;
