05ae8713d0e0706010720a50ebbce3104244fde8
Ty D'Angelo <tydangelo18@gmail.com>
2020-10-26 17:21:21 -0500

    General Code Formatting.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -15,71 +15,73 @@ const bcrypt = require('bcryptjs');
 // @desc Test Route
 // @access Public
 router.get('/', auth, async (req, res) => {
-    try {
-        const user = await User.findById(req.user.id).select('-password');
-        res.json(user);
-    } catch(err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route POST api/auth
 // @desc Authenticate User & get Token
 // @access Public
-router.post('/', 
-[
+router.post(
+  '/',
+  [
     check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Password is required').exists()
-],
- async (req, res) => {
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const { email, password } = req.body;
 
     try {
-    // See if the User exists
-    let user = await User.findOne({ email });
+      // See if the User exists
+      let user = await User.findOne({ email });
 
-    if(!user) {
-        return res.status(400).json({ errors: [ { msg: 'Invalid credentials' }] });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] });
+      }
+
+      // Match email and password
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] });
+      }
+
+      // Return the jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
     }
-
-    // Match email and password
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if(!isMatch) {
-        return res.status(400).json({ errors: [ { msg: 'Invalid credentials' }] });
-    }
-
-        // Return the jsonwebtoken
-        const payload = {
-            user: {
-                id: user.id
-            }
-        };
-
-        jwt.sign(
-            payload, 
-            config.get('jwtSecret'),
-            { expiresIn: 360000 },
-            (err, token) => {
-                if (err) throw err;
-                res.json({ token });
-                } 
-            );  
-    } catch(err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-
-    
-
-    
-});
+  }
+);
 
 // Export Route
 module.exports = router;
