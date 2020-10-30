61e315d95a3fa144e0a4395d7a69c0de4b49be57
Ty D'Angelo <tydangelo18@gmail.com>
2020-10-30 17:30:13 -0500

    Created a Redux Store.

---
+++ client/src/store.js
@@ -0,0 +1,20 @@
+// Redux boilerplate
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+// Root-Reducer connects to store
+import rootReducer from './reducers';
+
+// Initial State lives inside reducers
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+// Export the Redux Store for use
+export default store;
