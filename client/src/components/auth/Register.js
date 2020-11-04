65c2f91b1e638bcb37fb31ff807904eb0da508fa
Ty D'Angelo <tydangelo18@gmail.com>
2020-11-04 17:08:56 -0600

    Set up an alert system if user enters wrong password by building an alert component and connecting the alert reducer to it.

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     // Initial State
     name: '',
@@ -18,12 +21,12 @@ const Register = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
   // Form Submission Method
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Passwords do not match');
+      setAlert('Passwords do not match', 'danger');
     } else {
-      console.log(formData);
+      console.log('SUCCESS');
     }
   };
 
@@ -86,4 +89,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);
