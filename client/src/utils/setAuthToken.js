f8333462260c743fa47203f41901543fcfa928ef
Ty D'Angelo <tydangelo18@gmail.com>
2020-11-08 12:33:09 -0600

    Created Load User action and set our user authentication token to register a user.

---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+// The token from Local Storage is always sent from the request
+const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common['x-auth-token'] = token;
+  } else {
+    delete axios.defaults.headers.common['x-auth-token'];
+  }
+};
+
+export default setAuthToken;
