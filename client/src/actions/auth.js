59efdcf2630a774ead28fc94a44f4acd4fa253ca
Ty D'Angelo <tydangelo18@gmail.com>
2020-11-09 16:05:36 -0600

    Built Logout action and changed the links in the navbar that are shown to logout.

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,6 +8,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -77,4 +78,11 @@ export const login = (email, password) => async (dispatch) => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+// Logout User (Clear Profile)
+export const logout = () => (dispatch) => {
+  dispatch({
+    type: LOGOUT,
+  });
 };
