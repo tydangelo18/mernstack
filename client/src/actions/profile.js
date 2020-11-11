cccde4c2e9858f0e355f7b69b91df666693ced9e
Ty D'Angelo <tydangelo18@gmail.com>
2020-11-11 15:39:54 -0600

    Create a profile action to submit changes to profile.

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -4,18 +4,58 @@ import { setAlert } from './alert';
 import { GET_PROFILE, PROFILE_ERROR } from './types';
 
 // Get Current User's Profile
-export const getCurrentProfile = () => async dispatch => {
-    try {
-        const res = await axios.get('/api/profile/me');
+export const getCurrentProfile = () => async (dispatch) => {
+  try {
+    const res = await axios.get('/api/profile/me');
 
-        dispatch({
-            type: GET_PROFILE,
-            payload: res.data
-        });
-    } catch (err) {
-        dispatch({
-            type: PROFILE_ERROR,
-            payload: { msg: err.response.statusText, status: err.response.status }
-        });
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Create or Update Profile
+export const createProfile = (formData, history, edit = false) => async (
+  dispatch
+) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    // Post request to /api/profile
+    const res = await axios.post('/api/profile', formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+
+    // Alert based on if User creates profile or edits profile
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+
+    // Redirect User to Dashboard is creating a new profile
+    if (!edit) {
+      history.push('/dashboard');
     }
-}
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
