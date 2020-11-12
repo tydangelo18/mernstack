d0311fca6008a0f2554312f3a518e50e10ec93c4
Ty D'Angelo <tydangelo18@gmail.com>
2020-11-12 16:56:37 -0600

    Created Update profile reducer, created 2 new actions for adding experience and education, built 2 new components for adding experience and education.

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { setAlert } from './alert';
 
-import { GET_PROFILE, PROFILE_ERROR } from './types';
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from './types';
 
 // Get Current User's Profile
 export const getCurrentProfile = () => async (dispatch) => {
@@ -46,6 +46,67 @@ export const createProfile = (formData, history, edit = false) => async (
     if (!edit) {
       history.push('/dashboard');
     }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add Experience
+export const addExperience = (formData, history) => async (dispatch) => {
+  try {
+    // Put request to /api/profile/experience
+    const res = await axios.put('/api/profile/experience', formData);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+
+    // Alert based on if User creates profile or edits profile
+    dispatch(setAlert('Experience Added', 'success'));
+
+    // Redirect User to Dashboard is creating a new profile
+
+    history.push('/dashboard');
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add Education
+export const addEducation = (formData, history) => async (dispatch) => {
+  try {
+    // Put request to /api/profile/education
+    const res = await axios.put('/api/profile/education', formData);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+
+    // Alert based on if User creates profile or edits profile
+    dispatch(setAlert('Education Added', 'success'));
+
+    // Redirect User to Dashboard is creating a new profile
+    history.push('/dashboard');
   } catch (err) {
     const errors = err.response.data.errors;
 
