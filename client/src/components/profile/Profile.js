6260e2de2b6dc40434786b54f89d4ef0709093b8
Ty D'Angelo <tydangelo18@gmail.com>
2020-11-16 11:27:15 -0600

    Created Profile component as a parent to display all profile info.

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,53 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getProfileById } from '../../actions/profile';
+
+const Profile = ({
+  getProfileById,
+  profile: { profile, loading },
+  auth,
+  match,
+}) => {
+  // Will run immediately when the component mounts
+  useEffect(() => {
+    // get profile by the id in the URL (Destructure match to write as match.params.id rather than props.match.params.id)
+    getProfileById(match.params.id);
+  }, [getProfileById, match.params.id]);
+  return (
+    <Fragment>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Link to='/profiles' className='btn btn-light'>
+            Back To Profiles
+          </Link>
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user && (
+              <Link to='/edit-profile' className='btn btn-dark'>
+                Edit Profile
+              </Link>
+            )}
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+Profile.propTypes = {
+  getProfileById: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+  // see if the user is logged in
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { getProfileById })(Profile);
