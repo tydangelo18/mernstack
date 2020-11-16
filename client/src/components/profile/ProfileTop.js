8146518eec73b84714521b0416d67aaf4bf468f0
Ty D'Angelo <tydangelo18@gmail.com>
2020-11-16 17:42:53 -0600

    Simple format fix.

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -2,14 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ProfileTop = ({
-  // Destructure profile to get what we need for this component as props to pass into the return section below
   profile: {
+    user: { name, avatar },
     status,
     company,
     location,
     website,
     social,
-    user: { name, avatar },
   },
 }) => {
   return (
@@ -17,16 +16,16 @@ const ProfileTop = ({
       <img class='round-img my-1' src={avatar} alt='' />
       <h1 class='large'>{name}</h1>
       <p class='lead'>
-      {
+        {
           // If the profile has a company, display the status at company
-      }
+        }
         {status} {company && <span> at {company}</span>}
       </p>
       <p>{location && <span>{location}</span>}</p>
       <div class='icons my-1'>
-      {
+        {
           // If the profile has a website, display the website with an icon and a link to the website
-      }
+        }
         {website && (
           <a href={website} target='_blank' rel='noopener noreferrer'>
             <i class='fas fa-globe fa-2x'></i>
