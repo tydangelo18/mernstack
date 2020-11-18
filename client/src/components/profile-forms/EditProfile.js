1588a352d1ad60136d5e8d885f2721c6094a1111
Ty D'Angelo <tydangelo18@gmail.com>
2020-11-18 13:16:20 -0600

    Troubleshoot edit profile function.

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -32,7 +32,6 @@ const EditProfile = ({
   useEffect(() => {
     getCurrentProfile();
     setFormData({
-      // Needs to be refactored to understand why edit profile cannot be done on certain profiles.
       company: loading || !profile.company ? '' : profile.company,
       website: loading || !profile.website ? '' : profile.website,
       location: loading || !profile.location ? '' : profile.location,
@@ -41,14 +40,11 @@ const EditProfile = ({
       githubusername:
         loading || !profile.githubusername ? '' : profile.githubusername,
       bio: loading || !profile.bio ? '' : profile.bio,
-      twitter: loading || !profile.social.twitter ? '' : profile.social.twitter,
-      facebook:
-        loading || !profile.social.facebook ? '' : profile.social.facebook,
-      linkedin:
-        loading || !profile.social.linkedin ? '' : profile.social.linkedin,
-      youtube: loading || !profile.social.youtube ? '' : profile.social.youtube,
-      instagram:
-        loading || !profile.social.instagram ? '' : profile.social.instagram,
+      twitter: loading || !profile.social ? '' : profile.social.twitter,
+      facebook: loading || !profile.social ? '' : profile.social.facebook,
+      linkedin: loading || !profile.social ? '' : profile.social.linkedin,
+      youtube: loading || !profile.social ? '' : profile.social.youtube,
+      instagram: loading || !profile.social ? '' : profile.social.instagram,
     });
   }, [loading, getCurrentProfile]);
 
