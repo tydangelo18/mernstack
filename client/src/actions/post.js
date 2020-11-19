c5714eb66c9ada8e3044ef2e4378497d7c284f46
Ty D'Angelo <tydangelo18@gmail.com>
2020-11-19 15:55:44 -0600

    Delete Posts reducer, action, and implement into component.

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from '../actions/alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
+import { DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
 
 // Get post
 export const getPosts = () => async (dispatch) => {
@@ -40,6 +40,22 @@ export const removeLike = (id) => async (dispatch) => {
       type: UPDATE_LIKES,
       payload: { id, likes: res.data },
     });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+// Delete post
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/posts/${id}`);
+    dispatch({
+      type: DELETE_POST,
+      payload: id,
+    });
+    dispatch(setAlert('Post Removed', 'success'));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
