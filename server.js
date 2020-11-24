611efad41489ea385f2de1c192d99d5acde4b43a
Ty D'Angelo <tydangelo18@gmail.com>
2020-11-24 13:01:39 -0700

    Prepared for deployment.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 // Bring in MongoDB connection
 const connectDB = require('./config/db');
+const path = require('path');
 
 // Initialize App variable with express
 const app = express();
@@ -12,14 +13,21 @@ connectDB();
 // Init Middleware Bodyparser
 app.use(express.json({ extended: false }));
 
-// Test an endpoint to see if server and Express API is running
-app.get('/', (req, res) => res.send('API Running!'));
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve Static Assets in Production
+// Check for Production
+if (process.env.NODE_ENV === 'production') {
+  // Set Static Folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Take app variable and listen on Port 5000
 const PORT = process.env.PORT || 5000;
