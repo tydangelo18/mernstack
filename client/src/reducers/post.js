0b1374fdea28d302a8283106b055017b461f2564
Ty D'Angelo <tydangelo18@gmail.com>
2020-11-24 12:13:59 -0700

    Display comments in UI.

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -69,7 +69,7 @@ export default function (state = initialState, action) {
         ...state,
         post: {
           ...state.post,
-          comments: state.post.comment.filter(
+          comments: state.post.comments.filter(
             (comment) => comment._id !== payload
           ),
         },
