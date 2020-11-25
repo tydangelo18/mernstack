9e88e41f343aaa761eb0795b95a76f4d1150f3f9
Ty D'Angelo <tydangelo18@gmail.com>
2020-11-25 07:44:47 -0700

    Troubleshoot #2.

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-// Serve Static Assets in Production
+// Serve Static Assets in Production for UI
 // Check for Production
 if (process.env.NODE_ENV === 'production') {
   // Set Static Folder
